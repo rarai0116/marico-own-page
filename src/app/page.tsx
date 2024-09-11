b1a99d7c88113c5d3c8e5aa7b4dec85bbe166cb0
CommonMac <r.arai0116@gmail.com>
2024-09-11 19:16:34 +0900

    z-indexの設定を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import SideMenu from '@/components/parts/sideMenu';
 import WpParserElements from '@/components/parts/wpParserElements';
 
 const Home = async () => {
-  const post = await fetchPost('/pages/33').catch((error: unknown) => {
+  const post = await fetchPost('/pages/33', '/top').catch((error: unknown) => {
     console.error('Error:', error);
     throw new Error('Error fetching post');
   });
   return (
     <main>
       <SideMenu />
-      <div className="flex w-[90%] h-[100vh] pr-24 justify-center items-center">
+      <div className="flex w-[90%] h-[100vh] pr-24 justify-center items-center z-0">
         <WpParserElements rendered={post.content.rendered} />
       </div>
     </main>
